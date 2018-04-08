e0e8bd6d1ffe069157722610c4880f804ef63e40
Mark Vander Stel <mvndrstl@gmail.com>
2018-04-08 11:45:26 -0400

    Add more valid move checks
    Add the move checks for all pieces but the pawn.
    Add a function for checking if a spaces is a valid space (on the board).
    Change all storages of sets of spaces or moves to using Python sets.
    Make sure all move checks will return false if the start space is the
    same as the end space.

--- chess/moves.py
+++ chess/moves.py
@@ -1,18 +1,47 @@
 
+def space_on_board(x, y):
+  spaces = range(8)
+  return x in spaces and y in spaces
+
 def valid_knight_moves(x, y):
-  all_moves = [
+  all_moves = {
       (x + 2, y + 1), (x + 2, y - 1),
       (x - 2, y + 1), (x - 2, y - 1),
       (x + 1, y + 2), (x - 1, y + 2),
-      (x + 1, y - 2), (x - 1, y - 2)]
+      (x + 1, y - 2), (x - 1, y - 2)}
 
-  valid_moves = []
+  valid_moves = set()
   for _x, _y in all_moves:
-    if _x < 8 and _x >= 0 and _y < 8 and _y >= 0:
-      valid_moves.append((_x, _y))
+    if space_on_board(_x, _y):
+      valid_moves.add((_x, _y))
 
   return valid_moves
 
 def is_valid_knight_move(start_x, start_y, end_x, end_y):
+  if not space_on_board(end_x, end_y):
+    return False
   return (end_x, end_y) in valid_knight_moves(start_x, start_y)
+
+def is_valid_rook_move(start_x, start_y, end_x, end_y):
+  if not space_on_board(end_x, end_y):
+    return False
+  return (start_x != end_x) ^ (start_y != end_y)
+
+def is_valid_bishop_move(start_x, start_y, end_x, end_y):
+  if not space_on_board(end_x, end_y):
+    return False
+  dx = abs(start_x - end_x)
+  dy = abs(start_y - end_y)
+  return dx == dy and dx + dy != 0
+
+def is_valid_queen_move(start_x, start_y, end_x, end_y):
+  return (is_valid_rook_move(start_x, start_y, end_x, end_y)
+      or is_valid_bishop_move(start_x, start_y, end_x, end_y))
+
+def is_valid_king_move(start_x, start_y, end_x, end_y):
+  if not space_on_board(end_x, end_y):
+    return False
+  dx = abs(start_x - end_x)
+  dy = abs(start_y - end_y)
+  return dx <= 1 and dy <= 1 and dx + dy != 0
 
--- tests/test_moves.py
+++ tests/test_moves.py
@@ -4,10 +4,65 @@ import pytest
 
 from chess.moves import *
 
-def test_all_valid_knight():
-  assert valid_knight_moves(0, 0) == [(2, 1), (1, 2)]
+def test_space_on_board():
+  assert space_on_board(0, 0)
+  assert space_on_board(1, 1)
+  assert space_on_board(7, 7)
 
-def test_valid_knight():
+  assert not space_on_board(-1, 0)
+  assert not space_on_board(0, -1)
+  assert not space_on_board(-1, -1)
+
+  assert not space_on_board(8, 0)
+  assert not space_on_board(0, 8)
+  assert not space_on_board(8, 8)
+
+  assert not space_on_board(.5, 0)
+  assert not space_on_board('a', 1)
+
+def test_all_valid_knight_moves():
+  assert valid_knight_moves(0, 0) == {(2, 1), (1, 2)}
+  assert valid_knight_moves(1, 1) == {(0, 3), (2, 3), (3, 2), (3, 0)}
+  assert valid_knight_moves(3, 3) == {(5, 4), (5, 2), (1, 4), (1, 2),
+                                      (4, 5), (2, 5), (4, 1), (2, 1)}
+
+def test_valid_knight_move():
   assert is_valid_knight_move(3, 3, 2, 5)
   assert not is_valid_knight_move(3, 3, 2, 4)
+  assert not is_valid_knight_move(3, 3, 3, 3)
+  assert not is_valid_knight_move(0, 0, -2, -1)
+
+def test_valid_rook_move():
+  assert is_valid_rook_move(0, 0, 1, 0)
+  assert is_valid_rook_move(0, 0, 0, 1)
+  assert not is_valid_rook_move(0, 0, 1, 1)
+  assert not is_valid_rook_move(0, 0, 0, 0)
+  assert not is_valid_rook_move(0, 0, 8, 0)
+
+def test_valid_bishop_move():
+  assert is_valid_bishop_move(0, 0, 1, 1)
+  assert is_valid_bishop_move(0, 0, 7, 7)
+  assert is_valid_bishop_move(4, 3, 6, 5)
+  assert not is_valid_bishop_move(4, 3, 6, 7)
+  assert not is_valid_bishop_move(0, 0, 0, 0)
+  assert not is_valid_bishop_move(0, 0, 8, 8)
+  assert not is_valid_bishop_move(0, 0, -1, -1)
+
+def test_valid_queen_move():
+  assert is_valid_queen_move(0, 0, 1, 1)
+  assert is_valid_queen_move(0, 0, 0, 1)
+  assert is_valid_queen_move(0, 0, 1, 0)
+  assert not is_valid_queen_move(0, 0, 1, 2)
+  assert not is_valid_queen_move(0, 0, -1, 2)
+  assert not is_valid_queen_move(0, 0, 1, 8)
+
+def test_valid_king_move():
+  assert is_valid_king_move(0, 0, 1, 1)
+  assert is_valid_king_move(0, 0, 0, 1)
+  assert is_valid_king_move(0, 0, 1, 0)
+  assert not is_valid_king_move(0, 0, 2, 0)
+  assert not is_valid_king_move(0, 0, 0, 2)
+  assert not is_valid_king_move(0, 0, 3, 4)
+  assert not is_valid_king_move(0, 0, -1, 0)
+  assert not is_valid_king_move(0, 0, 0, -1)
 
